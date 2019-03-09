90568752c3da048d26866c1c1478800ab6010261
takemuratomoki <c611701554@tokushima-u.ac.jp>
2019-03-09 22:10:41 +0900

    [add]GetもしくはPostでTHHPから受信する関数を追加
    Test1.javaの追加、動作を確認

---
+++ CURLClone/src/main/java/curlclone/Test1.java
@@ -0,0 +1,77 @@
+package curlclone;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.lang.StringBuilder;
+
+public class Test1{
+	public static void main(String[] arg) {
+		GetAndPostRequest g = new GetAndPostRequest();
+		g.Getter();
+	}
+}
+
+class GetAndPostRequest {
+
+	void Getter() {
+		
+		String strUrl = "https://www.yahoo.co.jp/";
+		
+		HttpURLConnection urlConn = null;
+		InputStream in = null;
+		BufferedReader reader = null;
+		
+		try {
+			//url指定
+			URL url = new URL(strUrl);
+			
+			//コネクション取得
+			urlConn = (HttpURLConnection) url.openConnection();
+			
+			urlConn.setRequestMethod("GET");
+//			urlConn.setRequestMethod("POST");
+			
+			urlConn.connect();
+			
+			int status = urlConn.getResponseCode();
+			
+			System.out.println("HTTPステータス："+ status);
+			
+			if(status == HttpURLConnection.HTTP_OK) {
+				
+				in = urlConn.getInputStream();
+				
+				reader = new BufferedReader(new InputStreamReader(in));
+				
+				StringBuilder output = new StringBuilder();
+				String line;
+				
+				while((line = reader.readLine()) != null) {
+//					output.append(line);
+					System.out.println(line);
+				}
+				System.out.println(output.toString());
+	
+			}
+		}catch (IOException e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if(reader != null) {
+					reader.close();
+				}
+				if(urlConn != null) {
+					urlConn.disconnect();
+				}
+			}catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+
+}
